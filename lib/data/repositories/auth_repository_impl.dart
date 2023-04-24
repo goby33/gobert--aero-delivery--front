f43d465aa7782c93ba7315fdcd1d783fb0443d35
Goby33 <martin.gobert@hotmail.fr>
2023-04-24 09:59:48 +0200

    feat(auth): add firebase

---
+++ lib/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,7 @@
+import 'package:aero_delivery/data/sources/Auth_firebase.dart';
+
+class AuthRepositoryImpl {
+  final AuthFirebase _authFirebase;
+  AuthRepositoryImpl(this._authFirebase);
+
+}
---
+++ lib/data/sources/auth_firebase.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthFirebase{
+  static AuthFirebase? _instanceAuthFirebase;
+  static FirebaseAuth? _authFirebase;
+
+  AuthFirebase._();
+}
